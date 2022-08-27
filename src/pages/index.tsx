3214b019f5549ef8a4c8be0d39fc904397098631
Thorsten Herfurtner <thosten@alexanderherfurtner.de>
2022-08-27 16:32:54 +0200

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div>hello world</div>
+    </div>
+  );
+};
+
+export default Home;
