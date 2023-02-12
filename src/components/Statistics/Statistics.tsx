dffd2f299b32cbc84dca9cb65388f173296090b0
Thorsten Herfurtner <thorsten_herfurtner@gmx.de>
2023-02-12 17:36:11 +0100

    cleanup

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,41 +1,26 @@
 import React, { useEffect, useState } from "react";
 import Stat from "./components/Stat";
 
-const millisecondsInSecond = 1000;
-
 type Props = {};
 
 const Statistics = (props: Props) => {
   const durationTime = 100;
-  const framesPerSecond = 30;
   const updateInterval = 10; //millisecondsInSecond / framesPerSecond;
   const [elapsedTime, SetElapsedTime] = useState(0);
 
   useEffect(() => {
-    console.log("useEffect");
     let timeout: ReturnType<typeof setTimeout> | undefined = undefined;
-
-    if (elapsedTime === 0) console.time("bench");
 
     if (elapsedTime < durationTime) {
       timeout = setTimeout(() => {
-        console.log("timeout");
-        console.log("elapsedTime", elapsedTime);
         SetElapsedTime((current) => ++current);
       }, updateInterval);
-    } else {
-      console.timeEnd("bench");
     }
 
     return () => {
-      console.log("useEffect cleanup");
       clearTimeout(timeout);
     };
   }, [elapsedTime]);
-
-  useEffect(() => {
-    console.log("hello world");
-  });
 
   return (
     <div className="flex justify-center items-center p-5 gap-4">
