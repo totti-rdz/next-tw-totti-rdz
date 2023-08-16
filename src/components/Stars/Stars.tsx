db84fdefc9fb23aedab817522f67218745c089a0
totti-rdz <thorsten_herfurtner@gmx.de>
2023-08-17 01:33:27 +0200

    rename boolean variable starting star animation

--- src/components/Stars/Stars.tsx
+++ src/components/Stars/Stars.tsx
@@ -3,17 +3,17 @@ import AnimatedStar from "./components/AnimatedStar";
 import Quote from "./components/Quote";
 
 const index = () => {
-  const [bool, setBool] = useState(true);
-  const startAnimation = () => setBool((current) => !current);
+  const [startAnimation, setStartAnimation] = useState(true);
+  const toggleAnimation = () => setStartAnimation((current) => !current);
 
   return (
     <div className="flex flex-col justify-center items-center p-5 space-y-5">
-      <div className="flex" onClick={startAnimation}>
-        <AnimatedStar triggerAnimation={bool} delay={0.1} />
-        <AnimatedStar triggerAnimation={bool} delay={0.2} />
-        <AnimatedStar triggerAnimation={bool} delay={0.3} />
-        <AnimatedStar triggerAnimation={bool} delay={0.4} />
-        <AnimatedStar triggerAnimation={bool} delay={0.5} />
+      <div className="flex" onClick={toggleAnimation}>
+        <AnimatedStar triggerAnimation={startAnimation} delay={0.1} />
+        <AnimatedStar triggerAnimation={startAnimation} delay={0.2} />
+        <AnimatedStar triggerAnimation={startAnimation} delay={0.3} />
+        <AnimatedStar triggerAnimation={startAnimation} delay={0.4} />
+        <AnimatedStar triggerAnimation={startAnimation} delay={0.5} />
       </div>
       <Quote
         quote="The smartest, most hard-working and the most handsome young man out
