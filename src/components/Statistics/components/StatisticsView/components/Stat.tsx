c99e45abb9f922516925355ac5fe9651675d99e0
totti-rdz <thorsten_herfurtner@gmx.de>
2023-08-17 01:35:49 +0200

    change background color of Stat component

--- src/components/Statistics/components/StatisticsView/components/Stat.tsx
+++ src/components/Statistics/components/StatisticsView/components/Stat.tsx
@@ -14,7 +14,7 @@ type Props = TStat & {
 const Stat = ({ value, description, time, elapsedTime }: Props) => {
   const currentValue = Math.floor((value * elapsedTime) / time);
   return (
-    <div className="flex flex-col justify-between items-center text-center border-[#2597e8] bg-[#12aadc] border-2 p-4 pt-14 rounded-xl w-36 h-52 overflow-hidden">
+    <div className="flex flex-col justify-between items-center text-center border-[#facc15] bg-[#10aadc] border-2 p-4 pt-14 rounded-xl w-36 h-52 overflow-hidden">
       <span className="text-3xl font-bold text-[#facc15]">{currentValue}</span>
       <div className="flex justify-center items-center h-[3rem] ">
         <p className="text-white font-">{description}</p>
