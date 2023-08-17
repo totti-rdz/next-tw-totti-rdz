22c171d1ce3cc8d451a4e4e32abb6160a542418a
totti-rdz <thorsten_herfurtner@gmx.de>
2023-08-17 23:14:00 +0200

    add priority attribute to profile image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       <section className="relative bg-gradient-to-b from-blue-500 to-cyan-500">
         <div className="h-[65vh] w-full m-0 p-0 flex justify-center items-center text-center px-5">
           <div className="rounded-full overflow-hidden h-16 sm:h-20 md:h-24 w-16 sm:w-20 md:w-24 mx-3 shrink-0">
-            <Image src={profileImg} />
+            <Image src={profileImg} priority />
           </div>
           <h1 className="text-4xl sm:text-5xl md:text-7xl font-bold uppercase text-white shrink-1">
             <span className="bg-[url('../assets/text-background-waves.webp')] bg-cover bg-center text-transparent bg-clip-text hover:bg-clip-border hover:text-white rounded-lg px-0 sm:px-5">
