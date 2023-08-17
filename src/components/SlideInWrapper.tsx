b4c22b8cdd68eba31c277314ce65dd7588975adf
totti-rdz <thorsten_herfurtner@gmx.de>
2023-08-18 01:29:08 +0200

    add reverse property to SlideInWrapper

--- src/components/SlideInWrapper.tsx
+++ src/components/SlideInWrapper.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 type Props = {
   children: React.ReactNode;
+  reverse?: boolean;
   show?: boolean;
 };
 
-const SlideInWrapper = ({ children, show = true }: Props) => {
-  const translateXClass = show ? "translate-x-0" : "translate-x-[100vw]";
+const SlideInWrapper = ({ children, reverse = false, show = true }: Props) => {
+  const translateDirection = reverse ? "-translate-x-[100vw]" : "translate-x-[100vw]";
+  const translateXClass = show ? "translate-x-0" : translateDirection;
 
   return (
     <div className="w-full overflow-hidden">
