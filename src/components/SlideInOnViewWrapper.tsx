73246abf28da373f2521f2d2c0323ebfe280cf4d
totti-rdz <thorsten_herfurtner@gmx.de>
2023-08-18 16:45:07 +0200

    pass reverse prop from SlideInOnViewWrapper to SlideInWrapper

--- src/components/SlideInOnViewWrapper.tsx
+++ src/components/SlideInOnViewWrapper.tsx
@@ -4,13 +4,14 @@ import SlideInWrapper from "./SlideInWrapper";
 
 type Props = {
   children: React.ReactNode;
+  reverse?: boolean;
   triggerProp: string;
 };
 
-const SlideInOnViewWrapper = ({ children, triggerProp }: Props) => {
+const SlideInOnViewWrapper = ({ children, reverse, triggerProp }: Props) => {
   return (
     <TriggerOnViewWrapper triggerProp={triggerProp}>
-      <SlideInWrapper>{children}</SlideInWrapper>
+      <SlideInWrapper reverse={reverse}>{children}</SlideInWrapper>
     </TriggerOnViewWrapper>
   );
 };
