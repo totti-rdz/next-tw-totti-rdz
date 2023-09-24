de20a9536905e2fd50f052f1c0a69c477a63d48d
totti-rdz <thorsten_herfurtner@gmx.de>
2023-09-24 19:40:58 +0200

    fix Hydration error
    and client/server style prop mismatch in InfiniteHorizontalScrollWrapper

--- src/components/SkillScroller/components/InfiniteHorizontalScroller.tsx
+++ src/components/SkillScroller/components/InfiniteHorizontalScroller.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import InfiniteHorizontalScrollWrapper from "./InfiniteHorizontalScrollWrapper";
 import Tag from "./Tag";
 import { random } from "../helper/random";
@@ -16,21 +17,28 @@ const InfiniteHorizontalScroller = ({
   tags,
   tagsPerRow,
 }: Props) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    if (window !== undefined) setIsMounted(true);
+  }, []);
+
   return (
     <div className="w-[30rem] max-w-[90vw] flex flex-col shrink-0 gap-y-4 relative py-6 overflow-hidden">
-      {[...new Array(rows)].map((_, i) => (
-        <InfiniteHorizontalScrollWrapper
-          key={i}
-          duration={random(duration - 5000, duration + 5000)}
-          reverse={Boolean(i % 2)}
-        >
-          {shuffle(tags)
-            .slice(0, tagsPerRow)
-            .map((tag) => (
-              <Tag text={tag} key={tag} />
-            ))}
-        </InfiniteHorizontalScrollWrapper>
-      ))}
+      {isMounted &&
+        [...new Array(rows)].map((_, i) => (
+          <InfiniteHorizontalScrollWrapper
+            key={i}
+            duration={random(duration - 5000, duration + 5000)}
+            reverse={Boolean(i % 2)}
+          >
+            {shuffle(tags)
+              .slice(0, tagsPerRow)
+              .map((tag) => (
+                <Tag text={tag} key={tag} />
+              ))}
+          </InfiniteHorizontalScrollWrapper>
+        ))}
       <div className="pointer-events-none absolute inset-0 bg-[linear-gradient(90deg,#FEFEFA,transparent_30%,transparent_70%,#FEFEFA)]" />
     </div>
   );
